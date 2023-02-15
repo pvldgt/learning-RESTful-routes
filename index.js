f41b615fd738d6398ecddb734487c0051a794298
Pavel <pvldgt@gmail.com>
2023-02-15 14:29:25 +0300

    Update the index.js, remove silly code that was used at the beginning of the tutorial to show how get and post requests work

--- index.js
+++ index.js
@@ -101,15 +101,6 @@ app.delete("/comments/:id", (req, res) => {
     res.redirect("/comments")
 })
 
-app.get("/tacos", (req, res) => {
-    res.send("GET /tacos response")
-})
-
-app.post("/tacos", (req, res) => {
-    const { meat, qty } = req.body;
-    res.send(`OK, here are your ${qty} ${meat} tacos`)
-})
-
 app.listen(3000, () => {
     console.log("ON PORT 3000")
 })
